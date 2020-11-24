747928d4c48a38d153ccc5fbd2df3836d6afd84e
vdzeer <60068242+vdzeer@users.noreply.github.com>
2020-11-24 13:44:27 +0200

    any changes

--- api/server.js
+++ api/server.js
@@ -3,6 +3,6 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/:name', (req, res) => res.send(`Hello, ${req.params.name}!`));
+app.get('/:name', (req, res) => res.send(`Hello, dear ${(req.params.name)[0].toUpperCase() + (req.params.name).slice(1)}!`));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
