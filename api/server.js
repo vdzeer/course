863fe9bca4af5a6977df8234b801a5c246f17dde
vdzeer <60068242+vdzeer@users.noreply.github.com>
2020-12-04 16:29:31 +0200

    create error handlers and .env file (donenv)

--- api/server.js
+++ api/server.js
@@ -1,8 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/:name', (req, res) => res.send(`Hello, dear ${(req.params.name)[0].toUpperCase() + (req.params.name).slice(1)}!`));
+app.get('/user/:name', (req, res) => res.send(`Hello, dear ${(req.params.name)[0].toUpperCase() + (req.params.name).slice(1)}!`));
 
-app.listen(port, () => console.log(`Listening on port ${port}!`));
+app.use((req, res) => {
+  res.status(404).send('404: Page Not Found');
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send('500: Internal Server Error');
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
