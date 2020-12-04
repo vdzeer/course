15c9545ead2496d97ce8d3f0037951c383f2f0fa
vdzeer <60068242+vdzeer@users.noreply.github.com>
2020-12-04 16:55:59 +0200

    create class config

--- api/server.js
+++ api/server.js
@@ -1,7 +1,6 @@
-require('dotenv').config();
 const express = require('express');
+const cfg = require('./config');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/user/:name', (req, res) => res.send(`Hello, dear ${(req.params.name)[0].toUpperCase() + (req.params.name).slice(1)}!`));
@@ -14,4 +13,4 @@ app.use((err, req, res, next) => {
   res.status(500).send('500: Internal Server Error');
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
+app.listen(cfg.PORT, () => console.log(`Listening on port ${cfg.PORT}!`));
