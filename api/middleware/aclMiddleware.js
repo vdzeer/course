68723747d1f0b84653cdd38c09665479ca5604c4
vdzeer <vdzerniuk@gmail.com>
2021-02-05 00:56:57 +0200

    refactor aclmiddleware

--- api/middleware/aclMiddleware.js
+++ api/middleware/aclMiddleware.js
@@ -1,34 +1,33 @@
 const User = require('../models/user')
 const permissions = require('../models/permissions')
 
-module.exports = function ([...data]) {
+module.exports = function (rules) {
   return async function (req, res, next) {
-    const [admin, user] = data,
-      userID = req.user,
-      model = user.own.model,
-      column = user.own.column
-
-    const entity = await model.findByID(req.params.id)
-    if (!entity) {
-      return res.status(404).send(`This does not exist!`)
-    }
-
+    const userID = req.user
     const role = await User.getRole(userID)
     const userPermissions = permissions[role] || []
 
-    if (userPermissions.some((el) => el === admin.permission)) {
-      console.log('Welcome, admin!')
-      next()
+    let authorized = false
+
+    for await (const rule of rules) {
+      if (rule.own) {
+        const entity = await rule.own.model.findByID(req.params.id)
+        const entity_column = entity.rows[0][column]
+        if (
+          entity_column == userID &&
+          userPermissions.some((el) => el === rule.permission)
+        ) {
+          authorized = true
+        }
+      } else {
+        if (userPermissions.some((el) => el === rule.permission)) {
+          console.log('Welcome, admin!')
+          authorized = true
+        }
+      }
     }
 
-    const entity_column = entity.rows[0][column]
-    if (
-      entity_column == userID &&
-      userPermissions.some((el) => el === user.permission)
-    ) {
-      next()
-    } else {
-      return res.status(403).send(`This does not belong to you!`)
-    }
+    if (authorized) next()
+    else return res.status(403).send(`This does not belong to you!`)
   }
 }
