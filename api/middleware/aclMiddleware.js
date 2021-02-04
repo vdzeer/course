745e2bfbf23fe858d1bfc0ed87b437a095b2ad36
vdzeer <vdzerniuk@gmail.com>
2021-02-04 22:21:13 +0200

    Create roles to users

--- api/middleware/aclMiddleware.js
+++ api/middleware/aclMiddleware.js
@@ -1,17 +1,26 @@
+const User = require('../models/user')
+
 module.exports = function (model, column) {
   return async function (req, res, next) {
     const userID = req.user
     const entity = await model.findByID(req.params.id)
 
     if (!entity) {
-      return res.status(404).send(`This ${model} does not exist!`)
+      return res.status(404).send(`This does not exist!`)
     }
 
     const entity_column = entity.rows[0][column]
+    const role = await User.getRole(userID)
+
+    if (role.rows[0].role === 'admin') {
+      console.log('Welcome, admin!')
+      next()
+    }
+
     if (entity_column == userID) {
       next()
     } else {
-      return res.status(403).send(`This ${model} does not belong to you!`)
+      return res.status(403).send(`This does not belong to you!`)
     }
   }
 }
