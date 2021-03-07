2c131fb36137ccc564c16064d5884d9d8594aeb1
vdzeer <vdzerniuk@gmail.com>
2021-03-07 16:31:54 +0200

    Add api validator

---
+++ api/middleware/validator.js
@@ -0,0 +1,62 @@
+const db = require('../services/db')
+
+module.exports = function validator(data) {
+  return async (req, res, next) => {
+    for ([field, rules] of Object.entries(data)) {
+      for (rule of rules) {
+        const [objectRule, ...params] = rule.split(':')
+        switch (objectRule) {
+          case 'required':
+            if (!req.body[field]) {
+              res.status(400).send(`Field '${field}' is required!`)
+            }
+            break
+          case 'min':
+            const min = parseInt(params[0])
+            if (req.body[field].length < min) {
+              res
+                .status(400)
+                .send(`Field '${field}' is short! Min length is ${min}`)
+            }
+            break
+          case 'max':
+            const max = parseInt(params[0])
+            if (req.body[field].length > max) {
+              res
+                .status(400)
+                .send(`Field '${field}' is long! Max length is ${max}`)
+            }
+            break
+          case 'oneOf':
+            if (!params.includes(req.body[field])) {
+              res
+                .status(400)
+                .send(`Field '${field}' must be one of: ${params.join(', ')}`)
+            }
+            break
+          case 'login':
+            if (
+              !/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(
+                req.body[field]
+              )
+            ) {
+              res.status(400).send(`Field '${field}' not email!`)
+            }
+            break
+          case 'unique':
+            const fld = await db.query(`SELECT * FROM $1 where $2 = $3`, [
+              params[0],
+              params[1],
+              req.body[field],
+            ])
+            if (fld.rowCount) {
+              return res.status(400).send(`Field '${field}' is already exist!`)
+            }
+            break
+        }
+
+        return next()
+      }
+    }
+  }
+}
--- api/routes/posts.js
+++ api/routes/posts.js
@@ -3,19 +3,32 @@ const router = new Router()
 const checkAuth = require('../middleware/authMiddleware')
 const aclMiddleware = require('../middleware/aclMiddleware')
 const Post = require('../models/post')
+const validator = require('../middleware/validator')
 
 module.exports = (controller) => {
   router.get('/', controller.getAllPosts)
   router.get('/:id', controller.getPostByID)
-  // router.post('/', [checkAuth, controller.createPost])
-  router.post('/', controller.createPost)
+  router.post('/', [
+    checkAuth,
+    validator({
+      title: ['required', 'min:1', 'max:40'],
+      content: ['required', 'min:1', 'max:500'],
+      access: ['oneOf:onlyMe:friends:all'],
+    }),
+    controller.createPost,
+  ])
 
   router.put('/:id', [
-    // checkAuth,
-    // aclMiddleware([
-    //   { permission: 'updateAnyPost' },
-    //   { permission: 'updateOwnPost', own: { model: Post, column: 'user_id' } },
-    // ]),
+    checkAuth,
+    aclMiddleware([
+      { permission: 'updateAnyPost' },
+      { permission: 'updateOwnPost', own: { model: Post, column: 'user_id' } },
+    ]),
+    validator({
+      title: ['required', 'min:1', 'max:40'],
+      content: ['required', 'min:1', 'max:500'],
+      access: ['oneOf:onlyMe:friends:all'],
+    }),
     controller.updatePost,
   ])
   router.delete('/:id', [
